3c4ce9990e315335cba660e0b4be19e7ed573d96
Miraidaa <saparovamira.7534@gmail.com>
2023-10-07 23:54:49 +0200

    Create firstPractice.java

---
+++ firstPractice.java
@@ -0,0 +1,10 @@
+ // Create a program which reads the name of the user from the console, and then greets the user. You can use the following two commands:
+public class firstPractice 
+{   
+public static void main(String args[])   
+{   
+String getTheUser = System.console().readLine("Enter your name"); 
+System.console().printf("Hello %s %n", getTheUser );
+}   
+}  
+
